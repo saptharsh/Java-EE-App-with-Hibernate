e07a4f1fdcdaa902d90fc28c866ea8e46affd22c
saptharsh <saptharsh@gmail.com>
2016-01-27 16:00:59 -0600

    making sure that all changes are saved

--- src/org/app/model/User.java
+++ src/org/app/model/User.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -16,7 +17,7 @@ public class User implements Serializable {
 	 */
 	private static final long serialVersionUID = 387599223992430146L;
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	private String firstName;
 	private String middleName;
